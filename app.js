a4cbd65fed03ff95bf982ebabbf281034b67c94e
65160104 <152173850+65160104@users.noreply.github.com>
2024-10-16 17:32:35 +0700

    create index file

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var express = require("express");
 var cors = require("cors");
+var path = require("path");
 var app = express();
 
 app.use(cors());
 
+// เสิร์ฟไฟล์ index.html เมื่อเข้าถึงเส้นทาง '/'
 app.get("/", function (req, res, next) {
-  res.json("Hello world");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(3000, function () {
-  console.log("Start Server");
+  console.log("Start Server at http://localhost:3000");
 });
